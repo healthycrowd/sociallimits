b7c81a239525ba1e098d00375c4224e4db854f1b
Matt Wisniewski <hello@mattw.life>
2023-07-12 00:26:37 -0400

    add module

---
+++ sociallimits/__init__.py
@@ -0,0 +1,19 @@
+__version__ = "0.1.0"
+
+
+class SocialPlatform:
+    def __init__(caption_limit=None, tag_limit=None):
+        self.caption_limit = caption_limit
+        self.tag_limit = tag_limit
+
+
+all_platforms = {
+    'FACEBOOK': SocialPlatform(caption_limit=2200),
+    'INSTAGRAM': SocialPlatform(tag_limit=30),
+    'PINTEREST': SocialPlatform(caption_limit=2200),
+    'TUMBLR': SocialPlatform(caption_limit=2200, tag_limit=20),
+    'TWITTER': SocialPlatform(caption_limit=280),
+}
+
+for (name, platform) in all_platforms.items():
+    locals()[name] = platform
